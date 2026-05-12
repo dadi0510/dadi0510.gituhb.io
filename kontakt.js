8d3abc188d1ca5fc8f73b1e7d77ccea2d3b8fdb7
dadi0510 <david@gkt-gradin.hr>
2026-05-12 19:12:58 +0200

    Add files via upload

--- kontakt.js
+++ kontakt.js
@@ -1,34 +1,18 @@
 document.getElementById("forma-gumb").addEventListener("click", function () {
-    var ime = document.getElementById("ime").value.trim();
-    var email = document.getElementById("email").value.trim();
-    var poruka = document.getElementById("poruka").value.trim();
+    var ime = document.getElementById("ime").value;
+    var email = document.getElementById("email").value;
+    var poruka = document.getElementById("poruka").value;
 
-    var emailProvjera = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-
-    if (ime === "") {
+    if (!ime) {
         alert("Upiši ime i prezime.");
         return;
     }
-
-    if (email === "") {
+    if (!email) {
         alert("Upiši email adresu.");
         return;
     }
-
-    if (!emailProvjera.test(email)) {
-        alert("Upiši ispravnu email adresu.");
-        return;
-    }
-
-    if (poruka === "") {
-        alert("Upiši poruku.");
-        return;
-    }
-
-    if (poruka.length < 32) {
+    if (poruka.length < 20) {
         alert("Poruka mora imati najmanje 32 znaka.");
         return;
     }
-
-    alert("Poruka je uspješno poslana.");
 });
